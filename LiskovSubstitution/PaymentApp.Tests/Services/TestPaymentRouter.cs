2eed2442ebd2b48d7c8eb7481ef9a867375e6c7b
jesspena <jessicapenaflores10@gmail.com>
2025-06-02 15:48:32 -0400

    test

--- LiskovSubstitution/PaymentApp.Tests/Services/TestPaymentRouter.cs
+++ LiskovSubstitution/PaymentApp.Tests/Services/TestPaymentRouter.cs
@@ -15,11 +15,30 @@ public class TestPaymentRouter
     }
   }
 
+  public class DummyRefunder : IRefunder
+  { 
+    public bool Refunded { get; private set; }
+
+    public void Refund(decimal amount, string reference)
+    {
+        Refunded = true;
+    }
+  }
+
+  public class UserNotifier : IUserNotifier
+  {
+      public void Notify(string message) => Console.WriteLine(message);
+      public void ShowRefundNotSupported(string method) => Console.WriteLine($"Refund not supported for method {method}.");
+      public void ShowRefundSuccess(string reference) => Console.WriteLine($"Refund processed for {reference}.");
+  }
+
+
   [Fact]
   public void TryRefund_ShouldSkip_WhenNotSupported()
   {
     var dummyPayment = new DummyPayment();
-    var paymentRouter = new PaymentRouter([dummyPayment], []);
+    var notifier = new UserNotifier();
+    var paymentRouter = new PaymentRouter([dummyPayment], [], notifier);
 
     var result = paymentRouter.TryRefund("dummy", 100, "ref123");
 
@@ -27,5 +46,15 @@ public class TestPaymentRouter
   }
 
   // TODO: Implement a positive scenario for refund
-  
+  [Fact]
+    public void TryRefund_ShouldSucceed_WhenSupported()
+    {
+        var dummyRefunder = new DummyRefunder();
+        var notifier = new UserNotifier();
+        var paymentRouter = new PaymentRouter([], [dummyRefunder], notifier);
+
+        var result = paymentRouter.TryRefund("dummy", 50, "ref456");
+
+        Assert.True(result);
+    }
 }
