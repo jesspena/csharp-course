fe53c9e42fa49b9572a6be28b596af46fe346ee7
Bernarth <alberthbernarth75@gmail.com>
2025-06-05 18:11:28 -0400

    initialize exam2 good luck

---
+++ Exam2/FirstExcercise/EmployeeManagementSystem/Employee.cs
@@ -0,0 +1,9 @@
+namespace EmployeeManagementSystem;
+
+public class Employee
+{
+  public string Name { get; set; }
+  public string Type { get; set; }
+  public decimal BaseSalary { get; set; }
+  public decimal Bonus { get; set; }
+}
